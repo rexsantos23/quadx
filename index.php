03b5cf7dc46b3131dabb16aac47a920883e820e6
Rex <santos.rex22@gmail.com>
2018-05-08 22:49:58 +0800

    Clean index file

--- index.php
+++ index.php
@@ -2,28 +2,10 @@
 
 require './vendor/autoload.php';
 
-//URI Object
-$uri = Zend\Diactoros\ServerRequestFactory::fromGlobals()->getUri();
-
-$debug = new DebugBar\StandardDebugBar();
-$debug->getJavascriptRenderer()->setBaseUrl($uri->withPort('8000')->withPath('/'));
-
-//middleware
-$handler = GuzzleHttp\HandlerStack::create();
-
-/*$middleware = new GuzzleHttp\Profiling\Middleware(
-    new GuzzleHttp\Profiling\Debugbar\Profiler(
-        $debug->getCollector('time')
-    )
-);
-
-$handler->unshift($middleware);*/
-
-$controller = new Controller\OrderController($debug->getJavascriptRenderer());
+$controller = new Controller\OrderController();
 
 $response = $controller->index();
 
 print($response);
-//(new Zend\Diactoros\Response\SapiEmitter())->emit($response);
 
 ?>
